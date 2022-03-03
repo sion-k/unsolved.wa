162b09b445c6f348cb8a0b8e68c9185e99ebc02f
sion-k <ygonepiece@naver.com>
2022-03-04 00:52:45 +0900

    0.0.1 add function get last problem

--- get_unsolved.py
+++ get_unsolved.py
@@ -1,4 +1,5 @@
-import http.client, json, time
+import http.client, json, time, requests
+from bs4 import BeautifulSoup
 
 file = open("solved.txt", "r")
 solved = set()
@@ -17,16 +18,33 @@ def get_problem(id):
     headers = { 'Content-Type': "application/json" }
     conn.request("GET", "/api/v3/problem/lookup?problemIds=%s" % query_statement, headers=headers)
     res = conn.getresponse()
-    data = res.read()
-    if data.decode("utf-8") == "Not Found":
-        return
-    problems = json.loads(data.decode("utf-8"))
+    data = res.read().decode("utf-8")
+    if data == "Not Found" or data == "[]" or not data:
+        return []
+    problems = json.loads(data)
     return problems
 
-file = open("unsolved.json", "w", encoding="utf-8")
 
+def parse_last_problem(html):
+    soup = BeautifulSoup(html, "html.parser")
+    return int(soup.select("#problemset > tbody > tr:nth-child(1) > td.list_problem_id")[0].text)
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+added_problem_url = "https://www.acmicpc.net/problem/added"
+
+def get_last_problem():
+    time.sleep(overhead_time)
+    response = requests.get(added_problem_url, headers=headers)
+    if response.status_code != 200:
+        print("last_problem status_code : %d" % response.status_code)
+        return 0
+    last_problem = parse_last_problem(response.text)
+    return last_problem
+
+file = open("unsolved.json", "w", encoding="utf-8")
 index = 1000
-while True:
+last_problem = get_last_problem()
+while index <= last_problem:
     query_statement = []
     while len(query_statement) < 100:
         if not index in solved:
@@ -34,7 +52,6 @@ while True:
         index += 1
     print("query %s to %s..." % (query_statement[0], query_statement[-1]))
     problem = get_problem(query_statement)
-    if not problem : break
     for p in problem:
         file.write(json.dumps(p))
         file.write("\n")
